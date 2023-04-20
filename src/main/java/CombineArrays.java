ed688435402d2f489149e260f1210ca84344b01f
Ken Kudo <kkudo224@gmail.com>
2023-04-20 17:08:40 +0000

    Done

--- src/main/java/CombineArrays.java
+++ src/main/java/CombineArrays.java
@@ -8,7 +8,16 @@ public class CombineArrays {
      * @return an array containing the contents of arr1 followed by the contents of arr2.
      */
     public int[] combine(int[] arr1, int[] arr2){
-
-        return null;
+        int[] res = new int[arr1.length+arr2.length];
+        int idx = 0;
+        for (int i=0;i<arr1.length;i++) {
+            res[idx] = arr1[i];
+            idx++;
+        }
+        for (int i=0;i<arr2.length;i++) {
+            res[idx] = arr2[i];
+            idx++;
+        }
+        return res;
     }
 }
